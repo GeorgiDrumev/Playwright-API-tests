7860e40b6cbe71c42c9f433342a7f7d235931dfb
Georgi Drumev <gogodrumev20@gmail.com>
2026-04-13 08:49:25 +0300

    add testomat workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   retries: 1,
-  reporter: [['list'], ['html', { open: 'never' }]],
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+    ['@testomatio/reporter/lib/adapter/playwright.js', { apiKey: process.env.TESTOMATIO }],
+  ],
   use: {
     baseURL: 'https://sandbox.mockerito.com',
     extraHTTPHeaders: {
